f4c87b74dfbe29cb6fed3ba7420a2f0d69bea1e9
Prashant73743 <prashant.21911204@viit.ac.in>
2024-05-20 17:02:47 +0530

    03 commit

---
+++ 03tailwindProps/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 class="bg-green-400 aspect-square" mb-5 > test</h1>
+     <card />
+     </>
+  )
+}
+
+export default App
